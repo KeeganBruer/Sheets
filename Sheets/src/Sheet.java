6f4d1cbc6dfb76246622ad95b1f959f39c4e95c9
keega <keega@10.120.23.216>
2018-11-28 19:20:32 -0500

    Added feature for Pretty Printing in the console.

--- Sheets/src/Sheet.java
+++ Sheets/src/Sheet.java
@@ -1,6 +1,7 @@
 
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,6 +10,13 @@ public class Sheet {
 	private File sheetFile;
 	public Sheet(File file) {
 		this.sheetFile = file;
+		if (!this.sheetFile.exists()) {
+			try {
+				this.sheetFile.createNewFile();
+			} catch (IOException e) {
+				System.out.println("ERROR: Spreadsheet file does not exist and could not be created.");
+			}
+		}
 		this.importSpreadSheet(this.sheetFile);
 	}
 	
@@ -16,10 +24,10 @@ public class Sheet {
 		try {
 			return sheet.get(col).get(row);
 		} catch (Exception e) {
-			for (int c = sheet.size(); c < col; c++) {
+			for (int c = sheet.size()-1; c < col; c++) {
 				sheet.add(new ArrayList<Cell>());
 			}
-			for (int r = sheet.get(col).size(); r < row; r++) {
+			for (int r = sheet.get(col).size()-1; r < row; r++) {
 				Cell cell = new Cell("");
 				cell.setPosition(row, col);
 				sheet.get(col).add(cell);
@@ -73,18 +81,96 @@ public class Sheet {
 				currentColumnIndex += 1;
 				currentRowIndex += 1;
 			}
+			int currentLongest = this.getLongestRowSize();
+			for (int r = 0; r < this.sheet.size(); r++) {
+				for (int i = this.sheet.get(r).size(); i < currentLongest; i++) {
+					Cell cell = new Cell(" ");
+					cell.setPosition(r, i);
+					this.sheet.get(r).add(cell);
+				}
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public void printSheet() {
-		System.out.println(this.sheet.size() + " " + this.sheet.get(0).size());
 		for (ArrayList<Cell> list : this.sheet) {
 			for (Cell c : list) {
 				System.out.print("\"" + c.value + "\"");
 			}
 			System.out.println();
 		}
+	}
+	
+	public void prettyPrintSheet() {
+		int longestLength = 0;
+		for (ArrayList<Cell> list : this.sheet) {
+			for (Cell c : list) {
+				if (c.value.split("").length > longestLength)
+					longestLength = c.value.split("").length;
+			}
+		}
+		int counter = 0;
+		for (int i = 0; i < this.getLongestRowSize(); i++) {
+			String[] array = new String[longestLength];
+			int count = array.length-1;
+			for (int x = (counter+"").split("").length-1; x >= 0; x--) {
+				array[count] = (counter+"").split("")[x];
+				count--;
+			}
+			String printString = "";
+			for (String s : array) {
+				if (s == null) {
+					printString += " ";
+				} else {
+					printString += s;
+				}
+			}
+			System.out.print("| " + printString + " |");
+			counter++;
+		}
+		System.out.println();
+		for (int i = 0; i < this.getLongestRowSize(); i++) {
+			for (int j = 0; j < longestLength+4; j++) {
+				System.out.print("-");
+			}
+		}
+		System.out.println();
+		for (ArrayList<Cell> list : this.sheet) {
+			for (Cell c : list) {
+				String[] array = new String[longestLength];
+				int count = array.length-1;
+				for (int i = c.value.split("").length-1; i >= 0; i--) {
+					array[count] = c.value.split("")[i];
+					count--;
+				}
+				String printString = "";
+				for (String s : array) {
+					if (s == null) {
+						printString += " ";
+					} else {
+						printString += s;
+					}
+				}
+				System.out.print("| " + printString + " |");
+			}
+			System.out.println();
+			for (int i = 0; i < list.size(); i++) {
+				for (int j = 0; j < longestLength+4; j++) {
+					System.out.print("-");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	private int getLongestRowSize() {
+		int longest = 0;
+		for (ArrayList<Cell> list : this.sheet) {
+			if (list.size() > longest)
+				longest = list.size();
+		}
+		return longest;
 	}
 }
--- Sheets/src/SpreadsheetTestr.java
+++ Sheets/src/SpreadsheetTestr.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class SpreadsheetTestr {
 	public static void main(String[] args) {
-		Sheet newSheet = new Sheet(new File("temp.csv"));
-		newSheet.printSheet();
-		System.out.println(newSheet.getCell(2, 1).value);
+		Sheet newSheet = new Sheet(new File("users.csv"));
+		newSheet.prettyPrintSheet();
+		//System.out.println(newSheet.getCell(2, 1).value);
 	}
 }
